fb37884d8d6b5d7393d6da12dc515f7297cd92d0
Дарина Сергеева <darinasergeeva122@gmail.com>
2024-12-24 14:18:54 +0500

    база данных

---
+++ LB4_AnimeTitlesApp/Models/AnimeTitle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB4_AnimeTitlesApp.Models;
+
+public partial class AnimeTitle
+{
+    public int Id { get; set; }
+
+    public short IdAnimeType { get; set; }
+
+    public short IdGenre { get; set; }
+
+    public int IdModerator { get; set; }
+
+    public string OriginalName { get; set; } = null!;
+
+    public string Titlename { get; set; } = null!;
+
+    public short? YearOfRelese { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Poster { get; set; }
+
+    public short? CountOfseries { get; set; }
+
+    public short? Duration { get; set; }
+
+    public bool IsComplete { get; set; }
+
+    public string Studio { get; set; } = null!;
+
+    public virtual AnimeType IdAnimeTypeNavigation { get; set; } = null!;
+
+    public virtual Genre IdGenreNavigation { get; set; } = null!;
+
+    public virtual User IdModeratorNavigation { get; set; } = null!;
+}
---
+++ LB4_AnimeTitlesApp/Models/AnimeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB4_AnimeTitlesApp.Models;
+
+public partial class AnimeType
+{
+    public short Id { get; set; }
+
+    public string AnimeOfType { get; set; } = null!;
+
+    public virtual ICollection<AnimeTitle> AnimeTitles { get; set; } = new List<AnimeTitle>();
+}
